852582a641e81157e63a4fcdedf066bbf25a154e
AWWAL3421 <makavellimuhammad@gmail.com>
2024-04-07 03:34:42 -0700

    Max

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-def max_integer(my_list=None):
-    if not my_list:
-        return None
-    return max(my_list)
+def max_integer(my_list=[]):
+    if my_list == []:
+        return(None)
+    my_list.sort()
+    return(my_list[-1])

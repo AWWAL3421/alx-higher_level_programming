774c7e8c4d4896a704575f97e1a4a5163adb7026
AWWAL3421 <makavellimuhammad@gmail.com>
2024-07-06 03:45:47 -0700

    0-body_size.sh

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-# This script sends a request to a URL and displays the size of the response body in bytes.
-
+# This script sends a request to a URL and shows the size in bytes.
 curl -s "$1" | wc -c
-

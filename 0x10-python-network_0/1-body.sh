ea3a43724fd2e93c85ed223a7e655739303be950
AWWAL3421 <makavellimuhammad@gmail.com>
2024-07-06 04:15:25 -0700

    1-body.sh

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,20 +1,3 @@
 #!/bin/bash
-
 # This script takes in a URL, sends a GET request to the URL, and displays the body of the response
-
-# Take in the URL as argument
-url="$1"
-
-# Send GET request and store response body in a variable
-response=$(curl -s -w "\n%{http_code}" "$url")
-
-# Get status code from response
-status_code=$(echo "$response" | tail -n1)
-
-# If status code is 200, display response body
-if [[ "$status_code" == "200" ]]; then
-  body=$(echo "$response" | head -n -1)
-  echo "$body"
-else
-  echo "Error: Response status code is not 200"
-fi
+curl -s -w "\n" "$1" | grep -q "200" && curl -s "$1" || true
